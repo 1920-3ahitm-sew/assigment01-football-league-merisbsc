5cbe9d0e75a154c485868095a508f306e97cf34a
merisbsc <m.besic@students.htl-leonding.ac.at>
2019-09-13 10:51:03 +0200

    variables and getters init not fin

---
+++ src/main/java/at/htl/football/League.java
@@ -0,0 +1,10 @@
+package at.htl.football;
+
+import java.util.List;
+
+public class League {
+
+    private List<Team> teams = new List<Team>() {
+    }
+
+}
---
+++ src/main/java/at/htl/football/Team.java
@@ -0,0 +1,52 @@
+package at.htl.football;
+
+public class Team {
+
+    private String name;
+    private int points;
+    private int wins;
+    private int draws;
+    private int defeats;
+    private int goalsShot;
+    private int goalsReceived;
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    //region getter
+    public String getName() {
+        return name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public int getDraws() {
+        return draws;
+    }
+
+    public int getDefeats() {
+        return defeats;
+    }
+
+    public int getGoalsShot() {
+        return goalsShot;
+    }
+
+    public int getGoalsReceived() {
+        return goalsReceived;
+    }
+
+    public int getGoalDifference() {
+        return getGoalsShot() - getGoalsReceived();
+    }
+    //endregion
+
+
+}
